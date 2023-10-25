283b3e96a3fecd99a1b3dfe6013cbc225f83e2c2
user <user@LAPTOP-2DK3J1EI>
2023-10-24 20:23:15 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using S10_Laboratorio10_ConsolaNET.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10_Laboratorio10_ConsolaNET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int mPhone = 0;
+            int mPad = 0;
+            int mapBrook = 0;
+            int mWatch = 0;
+            int total = 0;
+            string objeto= "";
+            int opcion = 0;
+            do
+            {
+                if (opcion == 0 || opcion > 5) opcion = InterfazConsola.menuPrincipal(); //PARTE 1
+                if (opcion > 0 && opcion < 5)
+                {
+                    switch (opcion)
+                    {
+                        case 1: objeto = "mPhone 3000"; break;
+                        case 2: objeto = "mPad 3500"; break;
+                        case 3: objeto = "MAPBrook 3800"; break;
+                        case 4: objeto = "mWatch 8000"; break;
+                    }
+                    int opcion2 = 0;
+                    do
+                    {
+                        switch (opcion2)
+                        {
+                            default:
+                                opcion2 = InterfazConsola.registrar(objeto); //PARTE 2
+                                break;
+                            case 1:
+                                switch (opcion) //PARTE 4
+                                {
+                                    case 1: mPhone += InterfazConsola.registrarVenta(objeto); break;
+                                    case 2: mPad += InterfazConsola.registrarVenta(objeto); break;
+                                    case 3: mapBrook += InterfazConsola.registrarVenta(objeto); break;
+                                    case 4: mWatch += InterfazConsola.registrarVenta(objeto); break;
+                                }
+                                opcion2 = 0;
+                                break;
+                            case 2:
+                                switch (opcion) //PARTE 3
+                                {
+                                    case 1: mPhone -= InterfazConsola.registrarDevolucion(objeto); break;
+                                    case 2: mPad -= InterfazConsola.registrarDevolucion(objeto); break;
+                                    case 3: mapBrook -= InterfazConsola.registrarDevolucion(objeto); break;
+                                    case 4: mWatch -= InterfazConsola.registrarDevolucion(objeto); break;
+                                }
+                                opcion2 = 0;
+                                break;
+                            case 3: break;
+                        }
+                    } while (opcion2 != 3);
+                    opcion = 0;
+
+                }
+            } while (opcion != 5);
+            total = mPhone + mPad + mapBrook + mWatch;
+            InterfazConsola.cabezaFinPrograma(); //PARTE 5
+            Console.Write("mPhones  " + "| ".PadLeft(12, ' ') + mPhone + "\r\n");
+            Console.Write("mPads    " + "| ".PadLeft(12, ' ') + mPad + "\r\n");
+            Console.Write("MAPBrooks" + "| ".PadLeft(12, ' ') + mapBrook + "\r\n");
+            Console.Write("mWatches " + "| ".PadLeft(12, ' ') + mWatch + "\r\n");
+            Console.Write("--------------------------------\r\n");
+            Console.Write("Total    " + "| ".PadLeft(12, ' ') + total + "\r\n");
+            InterfazConsola.pieFinPrograma();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ interfaces/InterfazConsola.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10_Laboratorio10_ConsolaNET.interfaces
+{
+    public class InterfazConsola
+    {
+        public static int menuPrincipal() //PARTE 1
+        {
+            Console.Clear();
+            tituloPantalla("Productos de mPhone");
+            Console.Write(
+                "1: Ventas de mPhone 3000\r\n" +
+                "2: Ventas de mPad 3500\r\n" +
+                "3: Ventas de MAPBrook 3800\r\n" +
+                "4: Ventas de mWatch 8000\r\n" +
+                "5: Salir\r\n");
+            return getOpcion();
+        }
+        public static int registrar(string objeto) //PARTE 2
+        {
+            Console.Clear();
+            tituloPantalla("Registrar Venta de " + objeto);
+            Console.Write(
+                "1: Registrar Venta\r\n" +
+                "2: Registrar Devolución\r\n" +
+                "3: Menu Principal\r\n");
+            return getOpcion();
+        }
+        public static int registrarDevolucion(string objeto) //PARTE 3
+        {
+            Console.Clear();
+            tituloPantalla("Registrar devolución de:");
+            Console.Write(
+                "Se va a registrar la devolución de\r\n" +
+                "un " + objeto + " ¿Desea Continuar?\r\n" +
+                "1: Sí\r\n" +
+                "2: No\r\n");
+            int opcion = getOpcion();
+            switch (opcion)
+            {
+                case 1: return 1;
+                case 2: return 0;
+                default: opcion = registrarDevolucion(objeto); break;
+            }
+            return opcion;
+        }
+        public static int registrarVenta(string objeto) //PARTE 4
+        {
+            Console.Clear();
+            tituloPantalla("Registrar venta de:");
+            Console.Write(
+                "Se va a registrar la venta de un\r\n" +
+                objeto + " ¿Desea Continuar?\r\n" +
+                "1: Sí\r\n" +
+                "2: No\r\n");
+            int opcion = getOpcion();
+            switch (opcion)
+            {
+                case 1: return 1;
+                case 2: return 0;
+                default: opcion = registrarVenta(objeto); break;
+            }
+            return opcion;
+            
+        }
+        public static void cabezaFinPrograma() //PARTE 5_1
+        {
+            tituloPantalla("Reporte Final");
+            Console.Write(
+                "Productos Vendidos | Cantidad\r\n" +
+                "--------------------------------\r\n");
+        }
+        public static void pieFinPrograma() //PARTE 5_2
+        {
+            Console.Write(
+                "================================\r\n" +
+                "¡Hasta Luego!\n\n\n\n" +
+                "Diseñado Por: Diego León");
+        }
+        public static void tituloPantalla(string titulo)
+        {
+            Console.Write("================================\r\n" +
+                           titulo + "\r\n" +
+                           "================================\r\n");
+        }
+        public static int getOpcion()
+        {
+            Console.Write("================================\r\n" +
+                "Ingrese una opción: ");
+            string opcionTexto = Console.ReadLine();
+            if (int.TryParse(opcionTexto,out int aa) == false) { return 10; }
+            if (int.Parse(opcionTexto) < 0) { return 10; }
+            return int.Parse(opcionTexto);
+        }
+    }
+}
